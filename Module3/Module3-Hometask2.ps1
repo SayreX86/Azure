41c59da40bbcf9422d0426f8a6c9f04860cb67f4
SayreX86 <sayrex_gs@tut.by>
2018-01-22 00:35:25 +0300

    verbose

--- Module3/Module3-Hometask2.ps1
+++ Module3/Module3-Hometask2.ps1
@@ -1,4 +1,4 @@
 $RG = 'RG'
 Login-AzureRmAccount
-New-AzureRmResourceGroup -Name $RG -Location westeurope
-New-AzureRmResourceGroupDeployment -ResourceGroupName $RG -TemplateUri 'https://raw.githubusercontent.com/SayreX86/Azure/master/Module3/main.json'
+New-AzureRmResourceGroup -Name $RG -Location westeurope -Verbose
+New-AzureRmResourceGroupDeployment -ResourceGroupName $RG -TemplateUri 'https://raw.githubusercontent.com/SayreX86/Azure/master/Module3/main.json' -Verbose
