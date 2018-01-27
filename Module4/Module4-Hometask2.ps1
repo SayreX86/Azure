4569415b373008c4b7b6665c68e32499be55a183
SayreX86 <sayrex_gs@tut.by>
2018-01-28 02:41:18 +0300

    fixes

--- Module4/Module4-Hometask2.ps1
+++ Module4/Module4-Hometask2.ps1
@@ -6,14 +6,14 @@ $DSCConfigPath = 'https://raw.githubusercontent.com/SayreX86/Azure/master/Module
 $TempDSCCOnfigPath = "$env:TEMP\MyDsc.ps1"
 Login-AzureRmAccount
 New-AzureRmResourceGroup -Name $RG2 -Location $Location -Force -Verbose
+#Create StorageAccount to put DSC scripts
 New-AzureRmStorageAccount -ResourceGroupName $RG2 -Name $StorageName -Type Standard_LRS -Location $Location -Verbose
 Set-AzureRmCurrentStorageAccount -ResourceGroupName $RG2 -Name $StorageName -Verbose
+#Download DSC script from GitHub and publish it
 Invoke-WebRequest -Uri $DSCConfigPath -OutFile $TempDSCCOnfigPath
 Publish-AzureRmVMDscConfiguration -ConfigurationPath $TempDSCCOnfigPath -ResourceGroupName $RG2 -StorageAccountName $StorageName -Force -Verbose
-
-# get the URI with the SAS token
+# Get the SAS token
 $DSCSAS = New-AzureStorageBlobSASToken -Container 'windows-powershell-dsc' -Blob 'MyDsc.ps1.zip' -Permission r -ExpiryTime (Get-Date).AddHours(2000)
-
-# provide SAS token during deployment
+#Provide SAS token during deployment
 New-AzureRmResourceGroup -Name $RG -Location westeurope -Force -Verbose
 New-AzureRmResourceGroupDeployment -ResourceGroupName $RG -TemplateUri 'https://raw.githubusercontent.com/SayreX86/Azure/master/Module3/main.json' -DSC-SasToken $DSCSAS
