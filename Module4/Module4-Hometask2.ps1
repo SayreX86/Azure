94f4da11cb9ce1b92dbbde28870298c7b3ecc900
SayreX86 <sayrex_gs@tut.by>
2018-01-28 02:36:15 +0300

    fixes

--- Module4/Module4-Hometask2.ps1
+++ Module4/Module4-Hometask2.ps1
@@ -12,8 +12,7 @@ Invoke-WebRequest -Uri $DSCConfigPath -OutFile $TempDSCCOnfigPath
 Publish-AzureRmVMDscConfiguration -ConfigurationPath $TempDSCCOnfigPath -ResourceGroupName $RG2 -StorageAccountName $StorageName -Force -Verbose
 
 # get the URI with the SAS token
-$DSCSAS = New-AzureStorageBlobSASToken -Container 'windows-powershell-dsc' -Blob 'MyDsc.ps1.zip' -Permission r `
-  -ExpiryTime (Get-Date).AddHours(2000)
+$DSCSAS = New-AzureStorageBlobSASToken -Container 'windows-powershell-dsc' -Blob 'MyDsc.ps1.zip' -Permission r -ExpiryTime (Get-Date).AddHours(2000)
 
 # provide SAS token during deployment
 New-AzureRmResourceGroup -Name $RG -Location westeurope -Force -Verbose
