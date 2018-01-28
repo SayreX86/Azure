bdb05565f660eff9bb635c45c986eef28c93e64d
SayreX86 <sayrex_gs@tut.by>
2018-01-28 15:58:00 +0300

    check

--- Module4/Module4-Hometask2.ps1
+++ Module4/Module4-Hometask2.ps1
@@ -32,3 +32,6 @@ Write-Host "Please wait..." -BackgroundColor DarkCyan
 New-AzureRmResourceGroupDeployment -ResourceGroupName $RG -TemplateUri 'https://raw.githubusercontent.com/SayreX86/Azure/master/Module3/main.json' -DSC-SasToken $DSCSAS -Verbose
 #Check 8080 port access
 Test-NetConnection -ComputerName samoeunikalnoeimyavm.westeurope.cloudapp.azure.com -Port 8080
+$IE=new-object -com internetexplorer.application
+$IE.navigate2("http://samoeunikalnoeimyavm.westeurope.cloudapp.azure.com:8080/")
+$IE.visible=$true
