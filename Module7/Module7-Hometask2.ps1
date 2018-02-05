e2f5d25d705ddc84250a28113b2aa1ae2ccbb191
SayreX86 <sayrex_gs@tut.by>
2018-02-06 02:26:56 +0300

    fixes

--- Module7/Module7-Hometask2.ps1
+++ Module7/Module7-Hometask2.ps1
@@ -1,6 +1,5 @@
-$RG = 'RG4'
+$RG = 'RG3'
 Login-AzureRmAccount
-New-AzureRmResourceGroup -Name $RG -Location westeurope -Force -Verbose
 New-AzureRmResourceGroupDeployment -ResourceGroupName $RG `
 -TemplateUri 'https://raw.githubusercontent.com/SayreX86/Azure/master/Module7/vault.json' `
 -TemplateParameterUri 'https://raw.githubusercontent.com/SayreX86/Azure/master/Module7/vault.parameters.json' `
