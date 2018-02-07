368edbc48e57fcd3c3a778cb39dae75998231adc
Siarhei Hulitski <Siarhei.Hulitski@ipreo.com>
2018-02-07 15:10:24 +0300

    fixes

--- Module7/Module7-Hometask2BackupandRestore.ps1
+++ Module7/Module7-Hometask2BackupandRestore.ps1
@@ -1,20 +1,31 @@
-﻿#Trigger backup
+﻿Login-AzureRmAccount
+
+#Set AzureRecoveryService Context
+Get-AzureRmRecoveryServicesVault -Name 'RecoveryServicesVault' | Set-AzureRmRecoveryServicesVaultContext
 $namedContainer = Get-AzureRmRecoveryServicesBackupContainer -ContainerType "AzureVM" -Status "Registered"
 $item = Get-AzureRmRecoveryServicesBackupItem -Container $namedContainer -WorkloadType "AzureVM"
-$job = Backup-AzureRmRecoveryServicesBackupItem -Item $item
-$job
+#Trigger backup
+Backup-AzureRmRecoveryServicesBackupItem -Item $item
+while ((Get-AzureRmRecoveryServicesBackupJob -Operation Backup -Status InProgress) -ne $null)
+{
+    Write-Output "Waiting for backup..."
+}
 
 #Get AzureRM Disk
 $getazuredisk = Get-AzureRmDisk -ResourceGroupName 'RG3'
-#Set AzureRecoveryService Context
-Get-AzureRmRecoveryServicesVault -Name 'RecoveryServicesVault' | Set-AzureRmRecoveryServicesVaultContext
 #Get AzureRecovery Container
 $namedContainer = Get-AzureRmRecoveryServicesBackupContainer  -ContainerType "AzureVM" -Status "Registered"
 #Get AzureRecovery Item
 $backupitem = Get-AzureRmRecoveryServicesBackupItem -Container $namedContainer -WorkloadType "AzureVM"
 #Get AzureRecovery Point
 $rp = Get-AzureRmRecoveryServicesBackupRecoveryPoint $backupitem
+#Restore backup
+$restorejob = Restore-AzureRmRecoveryServicesBackupItem -RecoveryPoint $rp[0] -StorageAccountName 'savm2training' -StorageAccountResourceGroupName "RG3"
+while ((Get-AzureRmRecoveryServicesBackupJob -Operation Restore -Status InProgress) -ne $null)
+{
+    Write-Output "Waiting for restore..."
+}
 
-$restorejob = Restore-AzureRmRecoveryServicesBackupItem -RecoveryPoint $rp[0] -StorageAccountName 'backupsatraining' -StorageAccountResourceGroupName "RG3"
-$restorejob
+
+
 
