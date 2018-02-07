5a5b62eb873e7b3930bc04d8a4102cb92302377b
Siarhei Hulitski <Siarhei.Hulitski@ipreo.com>
2018-02-07 21:03:46 +0300

    fixes

--- Module7/Module7-Hometask2BackupandRestore.ps1
+++ Module7/Module7-Hometask2BackupandRestore.ps1
@@ -1,5 +1,9 @@
 ﻿Login-AzureRmAccount
 
+#Variables
+$SAName = 'savm2training'
+$RG = 'RG3'
+
 #Set AzureRecoveryService Context
 Get-AzureRmRecoveryServicesVault -Name 'RecoveryServicesVault' | Set-AzureRmRecoveryServicesVaultContext
 $namedContainer = Get-AzureRmRecoveryServicesBackupContainer -ContainerType "AzureVM" -Status "Registered"
@@ -12,7 +16,7 @@ while ((Get-AzureRmRecoveryServicesBackupJob -Operation Backup -Status InProgres
 }
 
 #Get AzureRM Disk
-$getazuredisk = Get-AzureRmDisk -ResourceGroupName 'RG3'
+$getazuredisk = Get-AzureRmDisk -ResourceGroupName $RG
 #Get AzureRecovery Container
 $namedContainer = Get-AzureRmRecoveryServicesBackupContainer  -ContainerType "AzureVM" -Status "Registered"
 #Get AzureRecovery Item
@@ -20,12 +24,36 @@ $backupitem = Get-AzureRmRecoveryServicesBackupItem -Container $namedContainer -
 #Get AzureRecovery Point
 $rp = Get-AzureRmRecoveryServicesBackupRecoveryPoint $backupitem
 #Restore backup
-$restorejob = Restore-AzureRmRecoveryServicesBackupItem -RecoveryPoint $rp[0] -StorageAccountName 'savm2training' -StorageAccountResourceGroupName "RG3"
+$restorejob = Restore-AzureRmRecoveryServicesBackupItem -RecoveryPoint $rp[0] -StorageAccountName $SAName -StorageAccountResourceGroupName $RG
 while ((Get-AzureRmRecoveryServicesBackupJob -Operation Restore -Status InProgress) -ne $null)
 {
     Write-Output "Waiting for restore..."
 }
 
+#Get restored files properties
+$GetLastRestore = Get-AzureRmRecoveryServicesBackupJob -Operation Restore 
+$GetLastRestireID = ($GetLastRestore | Select-Object -First 1).JobID
+$LastRestoreProperties = (Get-AzureRmRecoveryServicesBackupJobDetails -JobId $GetLastRestireID).Properties
+$storageAccountName = $LastRestoreProperties["Target Storage Account Name"]
+$containerName = $LastRestoreProperties["Config Blob Container Name"]
+$blobName = $LastRestoreProperties["Config Blob Name"]
 
+#Set storage context
+$Keylist = Get-AzureRmStorageAccountKey -ResourceGroupName $RG -StorageAccountName $SAName
+$Key = $Keylist[0].Value
+$Ctx = New-AzureStorageContext -StorageAccountName $SAName -StorageAccountKey $Key
 
-
+#Parsing JSON file
+New-Item -Path $env:TMP -Name Restorejson -ItemType Directory -Force
+Get-AzureStorageBlobContent -Container $containerName -Blob $blobName -Destination "$env:TMP\Restorejson" -Context $Ctx -Force
+$obj = ((Get-Content -Path "$env:TMP\Restorejson\$blobName" -Raw -Encoding Unicode)).TrimEnd([char]0x00) | ConvertFrom-Json
+#Prepare config for Disk
+$storageType = 'StandardLRS'
+$osDiskName = (Get-AzureRmVM -ResourceGroupName $RG -Name 'VM2').StorageProfile.OsDisk.Name
+Write-Host "OS Disk name is: $osDiskName" -BackgroundColor DarkCyan
+$osVhdUri = $obj.'properties.storageProfile'.osDisk.vhd.uri
+$diskConfig = New-AzureRmDiskConfig -AccountType $storageType -Location westeurope -CreateOption Import -SourceUri $osVhdUri
+$newRG = New-AzureRmResourceGroup -Name 'RG-DISK-COPY' -Location westeurope -Force
+#Create disk copy
+$osDisk = New-AzureRmDisk -DiskName $osDiskName -Disk $diskConfig -ResourceGroupName $newRG.ResourceGroupName
+Write-Host "The disk copy with name: $osDiskName is in resource group: $($newRG.ResourceGroupName)" -BackgroundColor DarkCyan
